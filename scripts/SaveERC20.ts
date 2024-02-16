f5757615802f1f4dd11386fcef9b5a381ad8291a
Ternder001 <nicholas.akura@gmail.com>
2024-02-16 14:42:25 +0100

    second commit

---
+++ scripts/SaveERC20.ts
@@ -0,0 +1,80 @@
+import {
+    loadFixture,
+  } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+//import { ethers } from 'ethers';
+// import { ethers } from "hardhat";
+// import { expect } from "chai";
+
+
+
+describe("SaveERC20", function() {
+
+  async function deploySaveERC20() {
+    let owner;
+    let addr1;
+    let addr2;
+
+    const SaveERC20 = await ethers.getContractFactory("SaveERC20");
+    [owner, addr1, addr2] = await ethers.getSigners();
+
+    const saveERC20 = await SaveERC20.deploy(addr1.address);
+
+    await saveERC20.waitForDeployment();
+
+    return { saveERC20, owner };
+  }
+
+  it("Should deposit tokens successfully", async function() {
+    const depositAmount = ethers.utils.parseEther("100");
+    const { saveERC20, owner } = await loadFixture(deploySaveERC20);
+    await expect(saveERC20.deposit(depositAmount))
+      .to.emit(saveERC20, "SavingSuccessful")
+      .withArgs(owner.address, depositAmount);
+  });
+
+  it("Should withdraw tokens successfully", async function() {
+    const depositAmount = ethers.utils.parseEther("100");
+    const withdrawAmount = ethers.utils.parseEther("50");
+    const { saveERC20, owner } = await loadFixture(deploySaveERC20);
+
+    await saveERC20.deposit(depositAmount);
+
+    await expect(saveERC20.withdraw(withdrawAmount))
+      .to.emit(saveERC20, "WithdrawSuccessful")
+      .withArgs(owner.address, withdrawAmount);
+  });
+
+  it("Should check user balance correctly", async function() {
+    const depositAmount = ethers.utils.parseEther("100");
+    const { saveERC20, owner } = await loadFixture(deploySaveERC20);
+
+    await saveERC20.deposit(depositAmount);
+
+    expect(await saveERC20.checkUserBalance(owner.address))
+      .to.equal(depositAmount);
+  });
+
+  it("Should check contract balance correctly", async function() {
+    const depositAmount = ethers.utils.parseEther("100");
+    const { saveERC20, owner } = await loadFixture(deploySaveERC20);
+
+    await saveERC20.deposit(depositAmount);
+
+    expect(await saveERC20.checkContractBalance())
+      .to.equal(depositAmount);
+  });
+
+  it("Should allow owner to withdraw tokens", async function() {
+    const depositAmount = ethers.utils.parseEther("100");
+    const withdrawAmount = ethers.utils.parseEther("50");
+    const { saveERC20, owner } = await loadFixture(deploySaveERC20);
+
+    await saveERC20.deposit(depositAmount);
+
+    await expect(saveERC20.connect(owner).ownerWithdraw(withdrawAmount))
+      .to.emit(saveERC20, "WithdrawSuccessful")
+      .withArgs(owner.address, withdrawAmount);
+  });
+});
